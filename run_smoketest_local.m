2e9c5b47bf71e67ac2b926e22404075a34192bfa
Foram Kamdar <foramkamdar23@gmail.com>
2026-02-25 14:48:19 -0800

    Stable hardware integration: EyeLink + BioSemi validated

--- run_smoketest_local.m
+++ run_smoketest_local.m
@@ -8,8 +8,10 @@ cfg.paths.scalesDir  = 'C:\Users\cns-co-admin\Desktop\fk\emotion_regulation_task
 cfg.paths.resultsDir = 'C:\Users\cns-co-admin\Desktop\fk\neurotoolbox\outputs';
 cfg.paths.assetsDir  = 'C:\Users\cns-co-admin\Desktop\fk\neurotoolbox\assets';
 
-cfg.trig.useTriggers = false;
-cfg.el.useEyelink    = true;
-%cfg.el.dummymode     = 1; 
+cfg.trig.portName = 'COM7';
+cfg.trig.useTriggers = true;
+cfg.trig.allowDummy  = false;
+%cfg.trig.pulseWidthSec = 0.010;
 
-Results = task_emoreg_run(cfg);
+cfg.el.useEyelink    = true;
+cfg.screen.screenNumber = 0; 
